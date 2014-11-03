2a18b691a504f2a46e4e4743ea43824b7c762b79
Sylvain Lobry <sylvain.lobry@telecom-paristech.fr>
2014-11-03 11:31:24 +0100

    Made the main and help message.
    	 * gitbib.py: Here.

---
+++ gitbib.py
@@ -0,0 +1,44 @@
+#! /usr/bin/python
+
+import sys
+
+def help():
+	print "python gitbib.py [config|add|search|edit|show|commit|push|help]"
+	print "- config: will create (or modify) configuration file gitbib.cf."
+	print "- add: will add a bib entry to the base."
+	print "- search: search a bibtex entry in the base given one keyword."
+	print "- edit: edit an entry given its name."
+	print "- show: show the file attached to an entry given its name."
+	print "- commit: commit modifications made to the base."
+	print "- push: push commits."
+	print "- help: show this message."
+
+def main(argv):
+	#Parse first argument and call the right function
+	if len(argv) > 1:
+		if argv[1] == "config":
+			config(argv)
+		elif argv[1] == "add":
+			add(argv)
+		elif argv[1] == "search":
+			search(argv)
+		elif argv[1] == "edit":
+			edit(argv)
+		elif argv[1] == "show":
+			show(argv)
+		elif argv[1] == "commit":
+			commit(argv)
+		elif argv[1] == "push":
+			push(argv)
+		elif argv[1] == "help":
+			help()
+		else:
+			help()
+			sys.exit(1)
+	else:
+		help()
+		sys.exit(1)
+	sys.exit(0)
+
+if __name__ == "__main__":
+	main(sys.argv)
